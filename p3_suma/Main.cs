eee89347163e7b1995196407374ec15b1a7e310b
Jonathan López <jlopez.cutonala@gmail.com>
2014-09-16 20:59:22 -0500

    Realizar la suma

--- p3_suma/Main.cs
+++ p3_suma/Main.cs
@@ -10,6 +10,9 @@ namespace p3_suma
 			int numero1= int.Parse(Console.ReadLine());
 			Console.WriteLine("Dame el segundo número:");
 			int numero2= int.Parse(Console.ReadLine());
+
+			int resultado = numero1 + numero2;
+			Console.WriteLine("El resultado es " + resultado);
 		}
 	}
 }
