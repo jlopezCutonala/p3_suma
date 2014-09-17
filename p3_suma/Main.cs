70fb3fb1eb13e729279e8af874229df37543b931
Jonathan López <jlopez.cutonala@gmail.com>
2014-09-16 20:56:51 -0500

    Pedir datos de entrada

--- p3_suma/Main.cs
+++ p3_suma/Main.cs
@@ -6,7 +6,10 @@ namespace p3_suma
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			Console.WriteLine("Dame el primer número:");
+			int numero1= int.Parse(Console.ReadLine());
+			Console.WriteLine("Dame el segundo número:");
+			int numero2= int.Parse(Console.ReadLine());
 		}
 	}
 }
